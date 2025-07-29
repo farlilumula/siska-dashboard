8eb97e5412ae1828ee4e2a06465d867f666993f4
Yogi Arismet <arismetyogi@gmail.com>
2025-07-29 23:57:02 +0700

    added Link to quick create button

--- src/components/nav-main.jsx
+++ src/components/nav-main.jsx
@@ -22,7 +22,7 @@ export function NavMain({
               tooltip="Create New Ticket"
               className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear">
               <IconCirclePlusFilled />
-              <span>Quick Create</span>
+              <Link to="/ticket/CreateTicket">Quick Create</Link>
             </SidebarMenuButton>
             <Button
               size="icon"
